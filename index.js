18e704bb404ec3b78ab7037ed736639873d6c8c0
Gabriel Torres <gabrieltlpires@gmai.com>
2023-04-10 22:38:39 -0300

    Adiciona solução do exercício divisão

---
+++ index.js
@@ -0,0 +1,4 @@
+let numerador = prompt ("Digite o numerador....:");
+let denominador = prompt ("Digite o denominador..:");
+let resultado = numerador / denominador;
+alert = `${numerador} dividido por ${denominador} é ${resultado}`;
