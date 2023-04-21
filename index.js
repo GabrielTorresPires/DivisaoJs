a717e9ef230674b81c97c6a7841d09dab7c9cfde
Gabriel Torres <gabrieltlpires@gmail.com>
2023-04-20 22:36:25 -0300

    Adiciona novos comandos

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-let numerador = prompt ("Digite o numerador....:");
-let denominador = prompt ("Digite o denominador..:");
-let resultado = numerador / denominador;
-alert = `${numerador} dividido por ${denominador} é ${resultado}`;
+let numerador = prompt ("Digite o numerador....:")
+let denominador = prompt ("Digite o denominador..:")
+if (denominador && numerador == 0)
+    {
+        alert("Não é possível dividir por 0")
+    } else 
+{
+let numero = Number(numerador)
+let divisor = Number(denominador)
+let resultado = numero / divisor
+alert( `O resultado de ${numero} divido por ${divisor} é: ${resultado}`);
+}
